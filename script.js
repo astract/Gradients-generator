c5c18d43293d7fa287a27b211902d8b7ad5d0114
Astract <astract@users.noreply.github.com>
2021-12-02 00:07:50 +0300

    Feedback about copy hex code
    After click to hex code show feedback status "Copied"

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-var r = document.querySelector(':root');
-
 // Create a function for make gradient
 function createGradient() {
+    var r = document.querySelector(':root');
     var randomColor_top = '#' + Math.floor(Math.random() * 16777215).toString(16);
     var randomColor_bottom = '#' + Math.floor(Math.random() * 16777215).toString(16);
     r.style.setProperty('--color_top', randomColor_top);
@@ -15,4 +14,12 @@ function createGradient() {
 // Click on Hex value for copy value
 function copyHexValue(id) {
     navigator.clipboard.writeText(document.getElementById(id).textContent);
+    // Save current hex value
+    let setValue = document.getElementById(id).textContent;
+
+    //  Show text "Copied" after click and return current hex value
+    document.getElementById(id).innerHTML = "Copied";
+    setTimeout(function () {
+        document.getElementById(id).innerHTML = setValue;
+    }, 300);
 }
