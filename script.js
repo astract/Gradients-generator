7fd8584cfe66d5542d8494f3d72ab4295ba4664e
Astract <astract@users.noreply.github.com>
2024-04-09 17:11:03 +0300

    GenerateColorArray
    После долгих упорст, я добавил функцию, которая на основе цветов в массиве применяет эти цвета на фон страницы

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 // Create a function for make gradient for background
 function createGradientByRGB() {
   let root = document.querySelector(":root");
@@ -36,6 +37,26 @@ function createGradientByRGB() {
   color_value_bottom.innerHTML = arrayColors[1];
   return arrayColors; // возвращает два хекс значения
 }
+ */
+
+function generateHexCode() {
+  const randomHex = Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + randomHex.padStart(6, "0");
+}
+
+// Function to generate an array with two hex codes
+function generateHexCodeArray() {
+  return [generateHexCode(), generateHexCode()];
+}
+
+function applyColorsToCSS() {
+  let color1 = generateHexCodeArray()[0];
+  let color2 = generateHexCodeArray()[1];
+  let root = document.querySelector(":root");
+  root.style.setProperty("--color_top", color1);
+  root.style.setProperty("--color_bottom", color2);
+  console.log("Color1: " + color1, "Color2: " + color2);
+}
 
 // Click on Hex value for copy value
 function copyHexValue(id) {
@@ -57,6 +78,7 @@ function copyHexValue(id) {
     });
 }
 
+/*
 // функция возвращает хекс код
 function generateHEXCode() {
   let redColor;
@@ -80,3 +102,4 @@ function generateHEXCode() {
 
   return (hexCode = redColor + greenColor + blueColor);
 }
+*/
